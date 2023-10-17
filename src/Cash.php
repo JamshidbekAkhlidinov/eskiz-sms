9f1acffdd87df2db8b3eb63371d82ab5bf9cfa93
JamshidbekAkhlidinov <jamshidbekakhlidinov@gmail.com>
2023-10-17 23:48:42 +0500

    create Cash trait

---
+++ src/Cash.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ustadev;
+
+trait Cash
+{
+    public function addCash($token)
+    {
+        if (!file_exists("cash")) {
+            mkdir("cash");
+        }
+
+        file_put_contents("cash/data.dat", $token);
+    }
+
+    public function getCash()
+    {
+        try {
+            $token = file_get_contents("cash/data.dat");
+            if (empty($token)) {
+                return false;
+            }
+            return $token;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+}
